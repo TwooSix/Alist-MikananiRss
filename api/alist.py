3b960ec9fd99fe212be332484ad1c6188e97d445
TwoSix <nitiaob@qq.com>
2023-04-25 17:58:50 +0800

    feat: 新增alist上传文件接口

--- api/alist.py
+++ api/alist.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import mimetypes
 
 
 class Alist:
@@ -61,3 +63,26 @@ class Alist:
         body = {"path": save_path, "urls": urls}
         response = requests.request("POST", api_url, headers=self.headers, json=body)
         return response.json()
+
+    def upload(self, save_path: str, file_path: str) -> dict:
+        """Upload file to Alist
+
+        Args:
+            save_path (str): save path start from /
+            file (str): file's path
+
+        Returns:
+            dict: response json data
+        """
+        assert self.isLogin, "Please login first"
+        api_url = f"https://{self.domain}/api/fs/put"
+        file_path = os.path.abspath(file_path)
+        file_name = os.path.basename(file_path)
+        headers = self.headers.copy()
+        mime_type = mimetypes.guess_type(file_name)[0]
+        headers["Content-Type"] = mime_type
+        headers["Content-Length"] = str(os.path.getsize("save.csv"))
+        headers["file-path"] = f"{save_path}/{file_name}"
+        with open(file_path, "rb") as f:
+            response = requests.put(api_url, headers=headers, data=f)
+            return response.json()
