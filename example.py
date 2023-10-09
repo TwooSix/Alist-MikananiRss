33f280dd228f64f12665e53f67710452341c4c96
TwoSix <nitiaob@qq.com>
2023-10-09 21:58:25 +0800

    更新readme，example，requirements

--- example.py
+++ example.py
@@ -1,12 +1,16 @@
-DOMAIN = "www.example.com"
-# DOMAIN = "localhost:5244" # 本地部署则为 localhost:port 或 127.0.0.1:port
+# Alist url
+BASE_URL = "http[s]://www.example.com"
+# BASE_URL = "http://127.0.0.1:port"
 
+# Alist 用户名和密码
 USER_NAME = "username"
 
 PASSWORD = "password"
 
+# 下载路径，相对于 Alist 根目录
 DOWNLOAD_PATH = "AliYun/Anime"
 
+# 正则表达式，用于过滤番剧
 REGEX_PATTERN = {
     "简体": r"(简体)|(简中)|(简日)|(CHS)",
     "繁体": r"(繁体)|(繁中)|(繁日)|(CHT)",
@@ -14,9 +18,11 @@ REGEX_PATTERN = {
     "非合集": r"^((?!合集).)*$",
 }
 
+# Mikan 订阅链接
 SUBSCRIBE_URL = "https://mikanani.me/RSS/MyBangumi?token=xxx"
 
-FILTERS = ["1080", "非合集"]
+# 实际使用的REGEX_PATTERN名字
+FILTERS = ["1080"]
 
 INTERVAL_TIME = 0  # 定时执行的间隔时间，单位为秒，0 为只运行一次
 
