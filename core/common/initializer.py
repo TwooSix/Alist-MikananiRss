01db283ae8bf1f48b4979c2ca9f1b7880efd59fe
TwoSix <nitiaob@qq.com>
2024-03-15 10:34:17 +0800

    chore: initializer

--- core/common/initializer.py
+++ core/common/initializer.py
@@ -15,7 +15,7 @@ config_loader = ConfigLoader("config.yaml")
 
 
 def setup_logger():
-    log_level = config_loader.get("dev.log_level", "INFO")
+    log_level = config_loader.get("dev.log_level", default="INFO")
     assert log_level in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
     logger.remove()
     logger.add("log/main_{time}.log", retention="7 days", level=log_level)
@@ -44,10 +44,9 @@ async def init_alist():
 
 
 def init_extrator():
-    try:
-        rename_cfg = config_loader.get("rename")
-    except KeyError:
-        return None
+    rename_cfg = config_loader.get("rename", None)
+    if rename_cfg is None:
+        raise KeyError("Try to init extractor but rename config not found")
     if "chatgpt" in rename_cfg:
         chatgpt_cfg = rename_cfg["chatgpt"]
         chatgpt = ChatGPT(
@@ -66,7 +65,7 @@ def init_extrator():
 def init_notification_bots():
     # init notification bot
     notification_bots = []
-    tg_config = config_loader.get("notification.telegram", False)
+    tg_config = config_loader.get("notification.telegram", None)
     if tg_config:
         bot_token = config_loader.get("notification.telegram.bot_token")
         user_id = config_loader.get("notification.telegram.user_id")
@@ -79,7 +78,7 @@ def init_regex_filter():
     # init resource filters
     regex_filter = RegexFilter()
     filters_name = config_loader.get("mikan.filters", [])
-    regex_pattern = config_loader.get("mikan.regex_pattern", {})
+    regex_pattern = config_loader.get("mikan.regex_pattern", None)
     regex_filter.update_regex(regex_pattern)
     for name in filters_name:
         regex_filter.add_pattern(name)
@@ -100,13 +99,8 @@ def init_mikan_rss_monitor(regex_filter: RegexFilter):
 
 def init_download_monitor(alist_client: Alist):
     download_path = config_loader.get("alist.download_path")
-    try:
-        rename_cfg = config_loader.get("rename")
-    except KeyError:
-        rename_cfg = None
-    use_renamer = False
-    if rename_cfg is not None:
-        use_renamer = True
+    rename_cfg = config_loader.get("rename", None)
+    use_renamer = False if rename_cfg is None else True
     download_monitor_thread = AlistDownloadMonitor(
         alist_client,
         download_path,
@@ -116,12 +110,7 @@ def init_download_monitor(alist_client: Alist):
 
 
 def init_alist_downloader(alist_client: Alist):
-    try:
-        rename_cfg = config_loader.get("rename")
-    except KeyError:
-        rename_cfg = None
-    use_renamer = False
-    if rename_cfg is not None:
-        use_renamer = True
+    rename_cfg = config_loader.get("rename", None)
+    use_renamer = False if rename_cfg is None else True
     downloader = AlistDownloader(alist_client, use_renamer)
     return downloader
