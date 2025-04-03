c0da5ac6aa8bec6697c38404f515826e1232b582
TwoooSix <nitiaob@qq.com>
2025-04-03 15:59:39 +0800

    chore: exmaple config filename

--- tests/common/test_config.py
+++ tests/common/test_config.py
@@ -3,9 +3,9 @@ from alist_mikananirss.common.config import ConfigManager
 
 def test_full_config_example():
     config = ConfigManager()
-    config.load_config("example_full.yaml")
+    config.load_config("full_config.yaml.example")
 
 
 def test_simple_config_example():
     config = ConfigManager()
-    config.load_config("example.yaml")
+    config.load_config("config.yaml.example")
